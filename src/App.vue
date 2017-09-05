ee2bc56fee339ab53d88003b029d35d9a4dc9a06
superDev610 <liaodingying610@hotmail.com>
2017-09-05 09:11:16 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Hello VueJS'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'Hi Ding';
+    }
+  }
+}
+</script>
