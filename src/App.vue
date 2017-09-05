2e8dbfc0087f20892f5bcf4f718c2cd9fee931e2
superDev610 <liaodingying610@hotmail.com>
2017-09-05 09:29:23 +0800

    Nesting component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <p>{{greeting()}}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
+
 export default {
-  name: 'app',
+  components: {
+    'ninjas': Ninjas
+  },
   data () {
     return {
-      title: 'Hello VueJS'
-    }
-  },
-  methods: {
-    greeting: function() {
-      return 'Hi Ding';
+      title: 'Ninja Apps'
     }
   }
 }
